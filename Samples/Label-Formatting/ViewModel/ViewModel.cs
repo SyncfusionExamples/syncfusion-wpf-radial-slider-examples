de4620a7d24c89f2bb44fc93caba44674d730d18
prabakaran.sangameswaran <prabakaran.sangameswaran@syncfusion.com>
2020-06-29 11:54:07 +0530

    Label-Formatting, Custom-Label sample committed.

---
+++ Samples/Label-Formatting/ViewModel/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Label_Formatting
+{
+    class ViewModel :NotificationObject
+    {
+        private ICommand drawLabelCommand;
+        public ICommand DrawLabelCommand
+        {
+            get
+            {
+                if (drawLabelCommand == null)
+                    drawLabelCommand = new UpdateLabel();
+                return drawLabelCommand;
+            }
+            set
+            {
+                drawLabelCommand = value;
+            }
+        }
+
+        public ViewModel()
+        {
+
+        }
+    }
+}
